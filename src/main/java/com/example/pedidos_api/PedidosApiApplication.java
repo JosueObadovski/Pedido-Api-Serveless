1f671213b1d7540aff9e7245fc9ed97203763bf8
JosueObadovski <101779621+JosueObadovski@users.noreply.github.com>
2025-03-02 15:38:53 -0300

    Atualização do projeto: Keycloak, Docker, CI/CD e AWS Lambda

--- src/main/java/com/example/pedidos_api/PedidosApiApplication.java
+++ src/main/java/com/example/pedidos_api/PedidosApiApplication.java
@@ -2,12 +2,13 @@ package com.example.pedidos_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.function.context.config.ContextFunctionCatalogAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {ContextFunctionCatalogAutoConfiguration.class}) // Excluindo configuração problemática
 public class PedidosApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(PedidosApiApplication.class, args);
 	}
-
 }
--- src/main/java/com/example/pedidos_api/lambda/PedidoLambdaHandler.java
+++ src/main/java/com/example/pedidos_api/lambda/PedidoLambdaHandler.java
@@ -1,6 +1,15 @@
 package com.example.pedidos_api.lambda;
 
-import org.springframework.cloud.function.adapter.aws.FunctionInvoker;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import org.springframework.stereotype.Component;
 
-public class PedidoLambdaHandler extends FunctionInvoker {
+@Component
+public class PedidoLambdaHandler implements RequestHandler<String, String> {
+
+    @Override
+    public String handleRequest(String input, Context context) {
+        context.getLogger().log("Recebido input: " + input);
+        return "Processado: " + input;
+    }
 }
